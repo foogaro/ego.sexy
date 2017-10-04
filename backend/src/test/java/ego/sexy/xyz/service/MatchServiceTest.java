b04cbde4051cc1f45a6cc63c7b6d4c8f4538df23
foogaro <l.fugaro@gmail.com>
2017-10-04 19:34:15 +0200

    Added the MatchService and the tried scaling the desire within the model.

--- backend/src/test/java/ego/sexy/xyz/service/MatchServiceTest.java
+++ backend/src/test/java/ego/sexy/xyz/service/MatchServiceTest.java
@@ -8,11 +8,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by foogaro on 04/10/17.
  */
 @RunWith(JUnit4.class)
 public class MatchServiceTest {
+
+    private MatchService matchService = new MatchService();
 
     @Test
     public void testMatchDesire() {
@@ -65,8 +70,76 @@ public class MatchServiceTest {
         Assert.assertFalse(collision > 0);
     }
 
-    public void testGeo() {
+    @Test
+    public void testScalingMatchDesire() {
 
+        Person x = new Person(new JsonObject()
+                .put("nickname", "X")
+                .put("age", 18)
+                .put("geo", new JsonObject())
+        );
+        x.setTake(Desire.BJ + Desire.HJ);
+
+        Person y = new Person(new JsonObject()
+                .put("nickname", "Y")
+                .put("age", 18)
+                .put("geo", new JsonObject())
+        );
+        y.setGive(128 + Desire.BJ + Desire.AFK);
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        int collision = x.getTake() & y.getGive();
+        System.out.println("collision = " + collision);
+        System.out.println("match = " + (collision > 0));
+        Assert.assertTrue(collision > 0);
+    }
+
+    @Test
+    public void testGetMatches() {
+        Person me = new Person(new JsonObject()
+                .put("nickname", "Me")
+                .put("age", 18)
+                .put("geo", new JsonObject())
+        );
+        me.setTake(Desire.BJ + Desire.AFK);
+        me.setGive(Desire.NS);
+
+        Person x = new Person(new JsonObject()
+                .put("nickname", "X")
+                .put("age", 18)
+                .put("geo", new JsonObject())
+        );
+        x.setTake(Desire.BJ + Desire.HJ);
+        x.setGive(Desire.AFK);
+
+        Person y = new Person(new JsonObject()
+                .put("nickname", "Y")
+                .put("age", 18)
+                .put("geo", new JsonObject())
+        );
+        y.setTake(Desire.FK + Desire.AFK);
+        y.setGive(Desire.FK + Desire.AFK);
+
+        Person z = new Person(new JsonObject()
+                .put("nickname", "Z")
+                .put("age", 18)
+                .put("geo", new JsonObject())
+        );
+        z.setTake(Desire.AFK);
+        z.setGive(Desire.NS);
+
+        List<Person> people = new ArrayList<>();
+        people.add(x);people.add(y);people.add(z);
+
+        List<Person> matchesr = new ArrayList<>();
+
+        for (Person person : people) {
+            if (matchService.match(me.getTake(), person.getGive())) {
+                System.out.println("Matcher: " + person);
+                matchesr.add(person);
+            }
+        }
     }
 
 }
